7ba3bd9fded5b96ee44edfb1826485d7a9877cd8
Ilya Gorban <ilyagorban@Ilyas-MacBook-Pro.local>
2021-05-09 15:52:40 +0300

    my additions

--- generate_aliases.py
+++ generate_aliases.py
@@ -54,6 +54,8 @@ def main():
         ('ing', 'ingress', ['g', 'd', 'rm'], None),
         ('cm', 'configmap', ['g', 'd', 'rm'], None),
         ('sec', 'secret', ['g', 'd', 'rm'], None),
+        ('j', 'job', ['g', 'd', 'rm'], None),
+        ('cj', 'cronjob', ['g', 'd', 'rm'], None),
         ('no', 'nodes', ['g', 'd'], ['sys']),
         ('ns', 'namespaces', ['g', 'd', 'rm'], ['sys']),
         ]
